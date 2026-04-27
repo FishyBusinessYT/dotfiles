afd2b50c919aa721c25884d0c338a912f1d55e7d
Cristóbal González <cristobalj.gonzalez@gmail.com>
2026-04-27 08:57:09 -0300

    Added install script

--- install.sh
+++ install.sh
@@ -1,19 +1,47 @@
-#!/bin/zsh
+#!/usr/bin/env bash
+set -e
 
+REPO="git@github.com:FishyBusinessYT/dotfiles.git"
+DOTFILES_DIR="$HOME/.dotfiles"
+function dots { git --git-dir="$DOTFILES_DIR/" --work-tree="$HOME" "$@" }
 cd $HOME
 
-echo Installing packages from deplist...
+
+echo "Cloning bare dotfiles repo..."
+git clone --bare $REPO $DOTFILES_DIR
+
+
+echo "Backing up any pre-existing files to ./.backup-files..."
+mkdir -p "$HOME/.backup-files"
+
+# Pipe error text through stdout to grep
+dots switch main 2>&1 | \
+    # Grep lines with filenames (they all start with spaces)
+    grep -E "^\s+" | \
+    # Remove whitespace from filenames
+    awk '{print $1}' | \
+    # Replace {} in the mv command with the filenames we got before
+    xargs -I{} mv "$HOME/{}" "$HOME/.backup-files/{}"
+
+
+echo "Copying dotfiles to home directory"
+dots switch main
+dots config --local status.showUntrackedFiles no
+
+
+echo "Installing packages from deplist..."
 if command -v pacman &> /dev/null; then
-    sudo pacman -S --noconfirm --needed - < ./deplist.txt
+    sudo pacman -S --needed - < ./deplist.txt
 elif command -v dnf &> /dev/null; then
     sudo dnf install $(cat ./deplist.txt)
 fi
 
+
+echo "Downloading and extracting fonts..."
 # Some setup
 mkdir -p ./tmpfls/unzipped
 mkdir -p $HOME/.local/share/fonts/
 
-echo Downloading and extracting fonts
 cd ./tmpfls
 wget -q --output-document rodondo.zip 'https://dl.dafont.com/dl/?f=rodondo'
 wget -q 'https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz'
@@ -32,12 +60,14 @@ rm *Semi*
 rm *Thin*
 find -type f -name '*JetBrains*' -not -name '*NL*' -delete
 
-echo Copying fonts and updating font cache...
+
+echo "Copying fonts and updating font cache..."
 mv -f * $HOME/.local/share/fonts
 fc-cache -f
 
-#Cleanup
+
+echo "Cleaning up..."
 cd ../..
 rm -rf ./tmpfls
 
-echo Done!
+echo "Done! System restart is recommended."
