12e3c8cba9d08259a75768d457ea9eecbb074591
Cristóbal González <cristobalj.gonzalez@gmail.com>
2026-05-19 19:16:45 -0300

    First setup

---
+++ .config/hypr/hyprland.lua
@@ -0,0 +1,14 @@
+-- Source other config files
+require("configs.input")
+require("configs.keybinds")
+require("configs.lookandfeel")
+require("configs.wrules")
+
+-- Monitor config
+hl.monitor({ output = "", mode = "preferred", position = "auto", scale = "1" })
+
+-- Default programs
+local terminal = "kitty"
+local browser = 'firefox'
+
+
