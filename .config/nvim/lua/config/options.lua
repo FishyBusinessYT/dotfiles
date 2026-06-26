3a78e169689751692c783588dbd4bc195f91cc00
Cristóbal González <cristobalj.gonzalez@gmail.com>
2026-06-26 09:05:57 -0300

    Even more options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -6,18 +6,12 @@ opt.relativenumber = true -- Show relative numbers for easy jumps
 opt.cursorline = true -- Highlight the line where the cursor's located
 opt.wrap = false -- Disable line wrapping
 opt.colorcolumn = '80,120' -- Mark columns 80 and 120 to help limit the length of single lines of code
+opt.list = true -- Display whitespace characters and trailing spaces
+opt.signcolumn = 'yes' -- Reserve a column for error icons and such
+opt.laststatus = 3 -- Have status line go across the whole window
 opt.scrolloff = 10 -- Keep a margin of 10 lines visible around the cursor when possible
 opt.sidescrolloff = 10 -- ...
 -- opt.scrolloffpad = 1 -- ... (Enable on next update)
-
-opt.list = true -- Display whitespace characters and trailing spaces
-opt.signcolumn = 'yes' -- Reserve a column for error icons and such
-opt.foldcolumn = 'auto' -- Amount of columns to reserve for folds
-opt.foldtext = '' -- Display the line's normal text when folded
-opt.fillchars = { -- Remove ugly dashes from folds
-    foldopen = '',
-    foldclose = '',
-}
 
 -- INDENTATION
 opt.shiftwidth = 4 -- Set indent size to 4
@@ -40,14 +34,23 @@ opt.completeopt = 'menu,popup,noinsert' -- Don't automatically insert text when 
 opt.iskeyword = '@,48-57,_,192-255,-' -- Treat dash as part of the word
 opt.clipboard = 'unnamedplus' -- Sync Neovim and system clipboard
 opt.mouse = '' -- Disable mouse
-opt.foldlevelstart = 99 -- Keep all folds open on file load
 opt.virtualedit = 'block' -- Allow cursor to move over empty space in visual block mode
 opt.timeoutlen = 200 -- How much to wait for a key sequence to complete
 
+-- FOLDS
+opt.foldlevelstart = 99 -- Keep all folds open on file load
+opt.foldcolumn = 'auto' -- Amount of columns to reserve for folds
+opt.foldtext = '' -- Display the line's normal text when folded
+opt.fillchars = { -- Remove ugly dashes from folds
+    foldopen = '',
+    foldclose = '',
+}
+
+-- SPLITS
+opt.splitbelow = true -- Horizontally split windows go down
+opt.splitright = true -- Vertically split windows go right
 
 -- COMPLETION
-
-
 
 vim.bo.indentexpr = 'v:lua.require("nvim-treesitter").indentexpr()' -- Treesitter indents
 vim.wo.foldmethod = 'expr' -- Use an expression for folds (useful for treesitter)
