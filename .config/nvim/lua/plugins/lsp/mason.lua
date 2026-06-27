f3b7226a927f059b09484b110edc39978bdd5347
Cristóbal González <cristobalj.gonzalez@gmail.com>
2026-06-27 13:38:50 -0300

    Updated Mason config and all plugins

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -1,31 +1,36 @@
-return {
-    {
-        'mason-org/mason.nvim',
-        opts = {
-            ui = {
-                icons = {
-                    package_installed = '✓',
-                    package_pending = '➜',
-                    package_uninstalled = '✗',
-                },
-            },
+---@module 'mason'
+---@type MasonSettings
+local mason_opts = {
+    firewall = {
+        enabled = true
+    },
+    ui = {
+        icons = {
+            package_installed = '✓',
+            package_pending = '➜',
+            package_uninstalled = '✗',
         },
     },
+}
+
+---@module 'mason-lspconfig'
+---@type MasonLspconfigSettings
+local mason_lspcfg_opts = {
+    automatic_enable = true,
+    ensure_installed = {
+        'lua_ls',
+        'stylua',
+        'pylsp',
+    },
+}
+
+return {
     {
         'mason-org/mason-lspconfig.nvim',
         dependencies = {
-            'mason-org/mason.nvim',
+            { 'mason-org/mason.nvim', opts = mason_opts },
             'neovim/nvim-lspconfig',
         },
-        ---@module 'mason'
-        ---@type MasonSettings
-        opts = {
-            automatic_enable = true,
-            ensure_installed = {
-                'lua_ls',
-                'stylua',
-                'pylsp',
-            },
-        },
+        opts = mason_lspcfg_opts,
     },
 }
