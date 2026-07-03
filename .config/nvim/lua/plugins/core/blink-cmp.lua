fec24e67b22326ccb67272e7298876ad38b05a14
Cristóbal González <cristobalj.gonzalez@gmail.com>
2026-07-03 12:28:35 -0300

    Touched up blink.nvim config

--- .config/nvim/lua/plugins/core/blink-cmp.lua
+++ .config/nvim/lua/plugins/core/blink-cmp.lua
@@ -2,29 +2,30 @@ return {
     'saghen/blink.cmp',
     version = '1.*',
     -- optional: provides snippets for the snippet source
-    --dependencies = { 'rafamadriz/friendly-snippets' },
+    dependencies = { 'rafamadriz/friendly-snippets' },
 
     ---@type blink.cmp.Config
     opts = {
         keymap = {
-            preset = 'super-tab',
+            preset = 'default',
+            ['<Tab>'] = { 'accept', 'fallback' },
         },
+
         completion = {
             keyword = { range = 'full' },
-            trigger = { show_in_snippet = false },
+            trigger = { show_on_insert = true }, -- Auto show
             list = { selection = { auto_insert = false } },
-            menu = { draw = { snippet_indicator = '' } },
+            menu = { draw = { snippet_indicator = ' >>' } },
+            ghost_text = { enabled = true },
+
             documentation = {
                 auto_show = true,
-                auto_show_delay_ms = 100,
-            },
-            ghost_text = {
-                enabled = true,
-                show_without_selection = true,
-                show_without_menu = false,
+                auto_show_delay_ms = 10,
             },
         },
+
         signature = { enabled = true },
         fuzzy = { implementation = 'rust' },
+        cmdline = { completion = { menu = { auto_show = true } } },
     },
 }
