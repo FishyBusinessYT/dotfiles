832d3bb8fe860495ab3ee8cb86a5c78d9c045c76
Cristóbal González <cristobalj.gonzalez@gmail.com>
2026-07-13 21:30:11 -0300

    Tweaked and commented cliphist script

--- .config/scripts/clipboard_history.sh
+++ .config/scripts/clipboard_history.sh
@@ -1,6 +1,11 @@
 #!/bin/zsh
-selection=$(cliphist list | fzf --no-sort | cliphist decode)
 
-if [ -n "$selection" ]; then
-	echo -n "$selection" | nohup wl-copy >/dev/null 2>&1
+selection=$(
+    cliphist list |  # Get cliphist items
+    fzf -d $'\t' --with-nth 2 |  # Don't display entry IDs
+    cliphist decode  # Get the original entry from the database
+)
+
+if [ -n "$selection" ]; then  # If a selection was made, copy to clipboard.
+    echo -n "$selection" | nohup wl-copy >/dev/null 2>&1
 fi
