624155daf5fc9d910bf96c18030f8139bf24ac09
Cristóbal González <cristobalj.gonzalez@gmail.com>
2026-07-14 11:43:44 -0300

    Commented media control script. Add playback controls as well.

--- .config/scripts/media_control.sh
+++ .config/scripts/media_control.sh
@@ -1,31 +1,37 @@
 #!/bin/zsh
 
-AUDIO_NOTIF_ID=9992
-TIMEOUT=1000
-INCREMENT=5%
+AUDIO_NOTIF_ID=9992  # Use a fixed ID so that notifications replace one another
+TIMEOUT_MS=1000  # Timeout for media control notifications
+INCREMENT=5%  # Increment for volume and brightness control
 ICONS_DIR=$HOME/.config/scripts/notif-icons
 
+# The first argument to this script dictates what it should do
 case $1 in
-    vol_up)
+    vol_up) # Every case is pretty much the same:
+        # Run operation
         wpctl set-volume -l 1 @DEFAULT_SINK@ $INCREMENT+
+
+        # Get resulting value
         volume=$(($(wpctl get-volume @DEFAULT_SINK@ | grep -Eo '[0-9]\.[0-9]*') * 100))
 
-        notify-send -t $TIMEOUT 'Volume increased' -h int:value:$volume -i $ICONS_DIR/volume-high.png -r $AUDIO_NOTIF_ID
+        # Send notification
+        notify-send -t $TIMEOUT_MS 'Volume increased' -h int:value:$volume -i $ICONS_DIR/volume-high.png -r $AUDIO_NOTIF_ID
         ;;
     vol_down)
         wpctl set-volume @DEFAULT_SINK@ $INCREMENT-
+
         volume=$(($(wpctl get-volume @DEFAULT_SINK@ | grep -Eo '[0-9]\.[0-9]*') * 100))
 
-        notify-send -t $TIMEOUT 'Volume lowered' -h int:value:$volume -i $ICONS_DIR/volume-low.png -r $AUDIO_NOTIF_ID
+        notify-send -t $TIMEOUT_MS 'Volume lowered' -h int:value:$volume -i $ICONS_DIR/volume-low.png -r $AUDIO_NOTIF_ID
         ;;
     vol_mute)
         wpctl set-mute @DEFAULT_SINK@ toggle
         isMuted=$(wpctl get-volume @DEFAULT_SINK@ | grep -c MUTE)
 
         if ((isMuted==1)); then
-            notify-send -t $TIMEOUT 'Audio muted' -i $ICONS_DIR/volume-mute.png -r $AUDIO_NOTIF_ID
+            notify-send -t $TIMEOUT_MS 'Audio muted' -i $ICONS_DIR/volume-mute.png -r $AUDIO_NOTIF_ID
         else
-            notify-send -t $TIMEOUT 'Audio unmuted' -i $ICONS_DIR/volume-high.png -r $AUDIO_NOTIF_ID
+            notify-send -t $TIMEOUT_MS 'Audio unmuted' -i $ICONS_DIR/volume-high.png -r $AUDIO_NOTIF_ID
         fi
         ;;
     mic_mute)
@@ -33,21 +39,21 @@ case $1 in
         isMuted=$(wpctl get-volume @DEFAULT_SOURCE@ | grep -c MUTE)
 
         if ((isMuted==1)); then
-            notify-send -t $TIMEOUT 'Microphone muted' -i $ICONS_DIR/mic-off.png -r $AUDIO_NOTIF_ID
+            notify-send -t $TIMEOUT_MS 'Microphone muted' -i $ICONS_DIR/mic-off.png -r $AUDIO_NOTIF_ID
         else
-            notify-send -t $TIMEOUT 'Microphone unmuted' -i $ICONS_DIR/mic.png -r $AUDIO_NOTIF_ID
+            notify-send -t $TIMEOUT_MS 'Microphone unmuted' -i $ICONS_DIR/mic.png -r $AUDIO_NOTIF_ID
         fi
         ;;
     brightness_up)
         brightnessctl s $INCREMENT+
         brightness=$(($(brightnessctl g) * 100 / $(brightnessctl m)))
 
-        notify-send -t $TIMEOUT 'Brightness increased' -h int:value:$brightness -i $ICONS_DIR/sunny-outline.png -r $AUDIO_NOTIF_ID
+        notify-send -t $TIMEOUT_MS 'Brightness increased' -h int:value:$brightness -i $ICONS_DIR/sunny-outline.png -r $AUDIO_NOTIF_ID
         ;;
     brightness_down)
         brightnessctl s $INCREMENT-
         brightness=$(($(brightnessctl g) * 100 / $(brightnessctl m)))
 
-        notify-send -t $TIMEOUT 'Brightness decreased' -h int:value:$brightness -i $ICONS_DIR/sunny-outline.png -r $AUDIO_NOTIF_ID
+        notify-send -t $TIMEOUT_MS 'Brightness decreased' -h int:value:$brightness -i $ICONS_DIR/sunny-outline.png -r $AUDIO_NOTIF_ID
         ;;
 esac
