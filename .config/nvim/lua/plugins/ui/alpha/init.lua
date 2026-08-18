f87fd1bb46d80ccd349ab78d08ccf4875fd9aaf5
Cristóbal González <cristobalj.gonzalez@gmail.com>
2026-08-18 08:19:11 -0300

    Factor out buttons logic

--- .config/nvim/lua/plugins/ui/alpha/init.lua
+++ .config/nvim/lua/plugins/ui/alpha/init.lua
@@ -1,45 +1,4 @@
--- Needs to be loaded after alpha itself.
--- local utils = require('alpha.utils') -- For getting the top level dir for a git repo
-local function make_button(shortcut, txt, callback)
-    local opts = { -- Button display options
-        position = 'center',
-        shortcut = shortcut,
-        cursor = -2,
-        width = 35,
-        align_shortcut = 'right',
-        hl = 'AlphaButtons',
-        hl_shortcut = 'AlphaShortcut',
-    }
-
-    -- Remove whitespace
-    shortcut = shortcut:gsub('%s', '')
-    if callback then
-        opts.keymap = { -- This is forwarded to vim.keymap.set by alpha
-            'n',
-            shortcut,
-            callback,
-            { noremap = true, silent = true, nowait = true },
-        }
-    end
-
-    local on_press
-    if type(callback) == 'function' then
-        on_press = callback
-    else
-        on_press = function()
-            local keys =
-                vim.api.nvim_replace_termcodes(callback, true, false, true)
-            vim.api.nvim_feedkeys(keys, 't', false)
-        end
-    end
-
-    return {
-        type = 'button',
-        val = txt,
-        on_press = on_press,
-        opts = opts,
-    }
-end
+local buttons = require('plugins.ui.alpha.buttons').buttons
 
 local header = {
     type = 'text',
@@ -57,27 +16,6 @@ local header = {
         position = 'center',
         hl = 'AlphaHeader',
     },
-}
-
-local buttons = {
-    type = 'group',
-    val = {
-        {
-            type = 'text',
-            val = 'Quick Menu',
-            opts = { hl = 'WarningMsg', position = 'center' },
-        },
-        { type = 'padding', val = 0 }, -- 2 spaces are still inserted, see 'spacing' below
-        make_button('e', '  New file', '<cmd>enew<CR>'),
-        make_button(
-            'c',
-            '  Configuration',
-            '<cmd>exe \'cd\' stdpath (\'config\')<CR>'
-        ),
-        make_button('u', '  Update plugins', '<cmd>Lazy sync<CR>'),
-        make_button('q', '󰅚  Quit', '<cmd>qa<CR>'),
-    },
-    opts = { spacing = 1 },
 }
 
 local projects = {
