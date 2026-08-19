8a507ef6d93bb64173dd85c5e6017837fe27a83b
Cristóbal González <cristobalj.gonzalez@gmail.com>
2026-08-19 15:05:22 -0300

    Piggyback: mini.pairs notes and godot setting

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,5 +1,8 @@
 return {
-    {
+    { -- Issues:
+        -- * Doesn't work for the <> pair.
+        -- * Say you have the following text: [ mini.pairs' ]. Inserting the
+        -- missing quote creates an entire pair.
         'nvim-mini/mini.pairs',
         opts = {
             modes = { insert = true, command = true, terminal = true },
